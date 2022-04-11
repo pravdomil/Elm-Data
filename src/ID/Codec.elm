4f36a6b0ceb23cd538e0c17c980d0263bb6dc04d
pravdomil <2387356+pravdomil@users.noreply.github.com>
2022-04-11 20:16:13 +0200

    fix codec

--- src/ID/Codec.elm
+++ src/ID/Codec.elm
@@ -6,14 +6,4 @@ import ID
 
 id : Codec.Codec (ID.ID a)
 id =
-    Codec.string
-        |> Codec.andThen
-            (\v ->
-                case ID.fromString v of
-                    Just b ->
-                        Codec.succeed b
-
-                    Nothing ->
-                        Codec.fail "Cannot decode ID."
-            )
-            ID.toString
+    Codec.string |> Codec.map ID.fromString ID.toString
