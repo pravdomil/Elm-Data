93ec3e95661c9518815305a9de3508990ad462e7
pravdomil <2387356+pravdomil@users.noreply.github.com>
2022-10-15 12:35:19 +0200

    next

--- src-example/Model.elm
+++ src-example/Model.elm
@@ -3,6 +3,9 @@ module Model exposing (..)
 import Codec
 import MemoryImage.FileImage
 import MemoryImage.FileSystem
+import Platform.Extra
+import Time
+import Time.Codec
 
 
 config : MemoryImage.FileSystem.Config Msg Model
@@ -26,13 +29,13 @@ config2 =
 
 
 type alias Model =
-    { counter : Int
+    { messages : List String
     }
 
 
 init : () -> ( Model, Cmd Msg )
 init () =
-    ( Model 0
+    ( Model [ "Welcome." ]
     , Cmd.none
     )
 
@@ -42,14 +45,23 @@ init () =
 
 
 type Msg
-    = IncreaseCounter
+    = NothingHappened
+    | LogTime Time.Posix
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        IncreaseCounter ->
-            ( { model | counter = model.counter + 1 }
+        NothingHappened ->
+            Platform.Extra.noOperation model
+
+        LogTime b ->
+            let
+                message : String
+                message =
+                    String.fromInt (Time.toSecond Time.utc b)
+            in
+            ( { model | messages = message :: model.messages }
             , Cmd.none
             )
 
@@ -60,8 +72,8 @@ update msg model =
 
 codec : Codec.Codec Model
 codec =
-    Codec.record (\x1 -> { counter = x1 })
-        |> Codec.field .counter Codec.int
+    Codec.record (\x1 -> { messages = x1 })
+        |> Codec.field .messages (Codec.list Codec.string)
         |> Codec.buildRecord
 
 
@@ -70,11 +82,15 @@ msgCodec =
     Codec.lazy
         (\() ->
             Codec.custom
-                (\fn1 x ->
+                (\fn1 fn2 x ->
                     case x of
-                        IncreaseCounter ->
+                        NothingHappened ->
                             fn1
+
+                        LogTime x1 ->
+                            fn2 x1
                 )
-                |> Codec.variant0 IncreaseCounter
+                |> Codec.variant0 NothingHappened
+                |> Codec.variant1 LogTime Time.Codec.posix
                 |> Codec.buildCustom
         )
