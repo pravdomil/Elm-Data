850614839e65ec41de05aa82426f5cda7ecf93b0
pravdomil <2387356+pravdomil@users.noreply.github.com>
2022-10-16 12:01:17 +0200

    next

--- src/RunningState.elm
+++ src/RunningState.elm
@@ -1,6 +1,24 @@
 module RunningState exposing (..)
 
+import Codec
+
 
 type RunningState
     = Running
     | Exiting
+
+
+codec : Codec.Codec RunningState
+codec =
+    Codec.custom
+        (\fn1 fn2 x ->
+            case x of
+                Running ->
+                    fn1
+
+                Exiting ->
+                    fn2
+        )
+        |> Codec.variant0 Running
+        |> Codec.variant0 Exiting
+        |> Codec.buildCustom
