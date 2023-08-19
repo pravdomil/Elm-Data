26f211e67aafdbd84c5bc975333a71b181d4ce3b
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-08-19 22:59:46 +0200

    next

---
+++ src/Ulid/Random.elm
@@ -0,0 +1,40 @@
+module Ulid.Random exposing (..)
+
+import JavaScript
+import Json.Decode
+import Json.Encode
+import Task
+import Time
+import Ulid
+
+
+generate : Task.Task x (Ulid.Ulid a)
+generate =
+    let
+        decoder : Json.Decode.Decoder ( Int, Int, Int )
+        decoder =
+            Json.Decode.map3
+                (\x x2 x3 -> ( x, x2, x3 ))
+                (Json.Decode.index 0 Json.Decode.int)
+                (Json.Decode.index 1 Json.Decode.int)
+                (Json.Decode.index 2 Json.Decode.int)
+
+        randomness : Task.Task x ( Int, Int, Int )
+        randomness =
+            JavaScript.run
+                "Array.from((typeof crypto === 'undefined' ? require('crypto').webcrypto : crypto).getRandomValues(new Int32Array(3)))"
+                Json.Encode.null
+                decoder
+                |> Task.onError
+                    (\_ ->
+                        JavaScript.run
+                            "[Math.random()*2**32, Math.random()*2**32, Math.random()*2**32]"
+                            Json.Encode.null
+                            decoder
+                    )
+                |> Task.onError (\_ -> Task.succeed ( 0, 0, 0 ))
+    in
+    Task.map2
+        Ulid.fromTimeAndRandomness
+        Time.now
+        randomness
