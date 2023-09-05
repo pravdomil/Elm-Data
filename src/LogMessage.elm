5f9dc28423a244c8e3e05bd6c3beed433388e058
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-09-05 20:47:26 +0200

    next

--- src/LogMessage.elm
+++ src/LogMessage.elm
@@ -8,29 +8,23 @@ import Json.Decode
 import Task
 
 
-type alias LogMessage =
-    { type_ : Type
-    , author : String
-    , messages : List String
-    }
+type LogMessage
+    = Info (List String)
+    | Warning (List String)
+    | Error (List String)
 
 
 log : LogMessage -> Task.Task JavaScript.Error ()
 log a =
-    let
-        fn : String -> Task.Task JavaScript.Error ()
-        fn =
-            case a.type_ of
-                Info ->
-                    Console.logInfo
+    case a of
+        Info _ ->
+            Console.logInfo (Codec.encodeToString codec a)
 
-                Warning ->
-                    Console.logWarning
+        Warning _ ->
+            Console.logWarning (Codec.encodeToString codec a)
 
-                Error ->
-                    Console.logError
-    in
-    fn (Codec.encodeToString codec a)
+        Error _ ->
+            Console.logError (Codec.encodeToString codec a)
 
 
 jsonDecodeError : Type -> String -> List String -> Json.Decode.Error -> LogMessage
@@ -47,43 +41,21 @@ javaScriptError a b c d =
 --
 
 
-type Type
-    = Info
-    | Warning
-    | Error
-
-
-
---
-
-
 codec : Codec.Codec LogMessage
 codec =
-    Codec.record (\x1 x2 x3 -> { type_ = x1, author = x2, messages = x3 })
-        |> Codec.field .type_ typeCodec
-        |> Codec.field .author Codec.string
-        |> Codec.field .messages (Codec.list Codec.string)
-        |> Codec.buildRecord
+    Codec.custom
+        (\fn1 fn2 fn3 x ->
+            case x of
+                Info x1 ->
+                    fn1 x1
 
+                Warning x1 ->
+                    fn2 x1
 
-typeCodec : Codec.Codec Type
-typeCodec =
-    Codec.lazy
-        (\() ->
-            Codec.custom
-                (\fn1 fn2 fn3 x ->
-                    case x of
-                        Info ->
-                            fn1
-
-                        Warning ->
-                            fn2
-
-                        Error ->
-                            fn3
-                )
-                |> Codec.variant0 Info
-                |> Codec.variant0 Warning
-                |> Codec.variant0 Error
-                |> Codec.buildCustom
+                Error x1 ->
+                    fn3 x1
         )
+        |> Codec.variant1 Info (Codec.list Codec.string)
+        |> Codec.variant1 Warning (Codec.list Codec.string)
+        |> Codec.variant1 Error (Codec.list Codec.string)
+        |> Codec.buildCustom
