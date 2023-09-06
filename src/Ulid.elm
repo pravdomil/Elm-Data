7e6bcb50bd9c970dacfd4f54e406ec9b5a2effa7
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-09-06 10:50:02 +0200

    next

--- src/Ulid.elm
+++ src/Ulid.elm
@@ -26,6 +26,10 @@ type Ulid a
 fromTimeAndRandomness : Time.Posix -> ( Int, Int, Int ) -> Ulid a
 fromTimeAndRandomness time ( random1, random2, random3 ) =
     let
+        encode18bits : Int -> String
+        encode18bits b =
+            String.padLeft 4 '0' (Result.withDefault "" (Crockford.encode (Bitwise.and 0x0003FFFF b)))
+
         encode20bits : Int -> String
         encode20bits b =
             String.padLeft 4 '0' (Result.withDefault "" (Crockford.encode (Bitwise.and 0x000FFFFF b)))
@@ -35,8 +39,8 @@ fromTimeAndRandomness time ( random1, random2, random3 ) =
             String.padLeft 6 '0' (Result.withDefault "" (Crockford.encode (Bitwise.and 0x3FFFFFFF b)))
     in
     Ulid
-        (encode30bits (Time.posixToMillis time // 0x00100000)
-            ++ encode20bits (Time.posixToMillis time)
+        (encode18bits (Time.posixToMillis time // 0x40000000)
+            ++ encode30bits (Time.posixToMillis time)
             --
             ++ encode30bits random1
             ++ encode30bits random2
