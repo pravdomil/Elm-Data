63aac069e6d5199b21e3bfbbf34a7a30ac04ad74
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-09-11 10:23:03 +0200

    next

--- src/Ulid/Random.elm
+++ src/Ulid/Random.elm
@@ -10,16 +10,16 @@ import Ulid
 
 generate : Task.Task x (Ulid.Ulid a)
 generate =
-    Task.map2 Ulid.fromTimeAndRandomness Time.now randomInts
+    Task.map2 Ulid.fromTimeAndRandomness Time.now randomness
 
 
-randomInts : Task.Task x ( Int, Int, Int )
-randomInts =
+randomness : Task.Task x Ulid.Randomness
+randomness =
     let
-        decoder : Json.Decode.Decoder ( Int, Int, Int )
+        decoder : Json.Decode.Decoder Ulid.Randomness
         decoder =
             Json.Decode.map3
-                (\x x2 x3 -> ( x, x2, x3 ))
+                Ulid.Randomness
                 (Json.Decode.index 0 Json.Decode.int)
                 (Json.Decode.index 1 Json.Decode.int)
                 (Json.Decode.index 2 Json.Decode.int)
@@ -35,4 +35,4 @@ randomInts =
                     Json.Encode.null
                     decoder
             )
-        |> Task.onError (\_ -> Task.succeed ( 0, 0, 0 ))
+        |> Task.onError (\_ -> Task.succeed (Ulid.Randomness 0 0 0))
